3c5167e956a2d2296ddfe70bce1874420734f7d5
Omar-Alme <omar.almehdar17@gmail.com>
2024-04-24 20:13:22 +0200

    tuples and enums

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -690,3 +690,88 @@ function isManager(obj: Person | DogOwner | Manager): obj is Manager {
 if (isManager(employee)){
     employee.delegateTasks();
 }
+
+
+// Tuples
+let human: [string, number] = ['emily', 3];
+human.push(5, 'adam')
+console.log(human)
+
+
+
+// Enums
+
+enum ServerResponseStatus {
+    Success = 200,
+    Error = 'Error',
+  }
+  
+  interface ServerResponse {
+    result: ServerResponseStatus;
+    data: string[];
+  }
+  
+  function getServerResponse(): ServerResponse {
+    return {
+      result: ServerResponseStatus.Success,
+      data: ['first item', 'second item'],
+    };
+  }
+  
+  const response: ServerResponse = getServerResponse();
+  console.log(response);
+
+
+  // Reverse nmapping - enumss
+  enum NumericEnum {
+    Member = 1,
+  }
+  
+  enum StringEnum {
+    Member = 'Value',
+  }
+  
+  let numericEnumValue: NumericEnum = 1; // This is allowed
+  console.log(numericEnumValue); // 1
+  
+  //let stringEnumValue: StringEnum = 'Value'; // This is not allowed
+
+// challenge
+/*
+- Define an enum named UserRole with members Admin, Manager, and Employee.
+- Define a type alias named User with properties id (number), name (string), 
+role (UserRole), and contact (a tuple with two elements: email as string and 
+    phone as string).
+- Define a function named createUser that takes a User object as its parameter 
+and returns a User object.
+- Call the createUser function with an object that matches the User type, 
+store the result in a variable, and log the variable to the console.
+*/
+
+enum UserRole{
+    Admin,
+    Manage,
+    Employee,
+}
+
+type Users = {
+    id: number;
+    name: string;
+    role: UserRole;
+    contact:[string, string];
+ };
+
+
+ function createUsers(users:Users): Users{
+    return users;
+
+ } 
+
+ const users:Users = createUsers({
+    id:1, 
+    name:'alan smith',
+    role: UserRole.Admin,
+    contact: ['alan@gmail.com', '123-123-123']
+ });
+
+ console.log(users)
