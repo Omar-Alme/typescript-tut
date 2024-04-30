bf55c9949446a3a3d41c733eeb5ba1b50478ef16
Omar-Alme <omar.almehdar17@gmail.com>
2024-04-30 11:22:49 +0200

    type assertion,unknown,never

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -78,10 +78,10 @@ requestStatus = 'pending'; // OK
 // This can be useful when you don't know the type of a variable, or when you want to opt out of type checking.
 // Here's an example of the any type in TypeScript:
 
-let unknownValue: any = 'Alice'; // Type is 'any'
-unknownValue = 42; // OK
-unknownValue = true; // OK
-unknownValue = ['Alice', 42, true]; // OK
+let unknownValues: any = 'Alice'; // Type is 'any'
+unknownValues = 42; // OK
+unknownValues = true; // OK
+unknownValues = ['Alice', 42, true]; // OK
 
 
 // Practical Example
@@ -775,3 +775,125 @@ type Users = {
  });
 
  console.log(users)
+
+
+ // Type Assertion
+ let someValue: any = 'This is a string';
+
+// Using type assertion to treat 'someValue' as a string
+let strLength: number = (someValue as string).length;
+
+type Bird = {
+  name: string;
+};
+
+// Assume we have a JSON string from an API or local file
+let birdString = '{"name": "Eagle"}';
+let dogString = '{"breed": "Poodle"}';
+
+//
+
+// Parse the JSON string into an object
+let birdObject = JSON.parse(birdString);
+let dogObject = JSON.parse(dogString);
+
+// We're sure that the jsonObject is actually a Bird
+let bird = birdObject as Bird;
+let dog = dogObject as Bird;
+
+console.log(bird.name);
+console.log(dog.name);
+
+enum Status {
+  Pending = 'pending',
+  Declined = 'declined',
+}
+
+type Userss = {
+  name: string;
+  status: Status;
+};
+// save Status.Pending in the DB as a string
+// retrieve string from the DB
+const statusValue = 'pending';
+
+const user: Userss = { name: 'john', status: statusValue as Status };
+
+
+
+// Type Uknown
+let unknownValue: unknown;
+
+// Assign different types of values to unknownValue
+unknownValue = 'Hello World'; // OK
+unknownValue = [1, 2, 3]; // OK
+unknownValue = 42.3344556; // OK
+
+// unknownValue.toFixed( ); // Error: Object is of type 'unknown'
+
+// Now, let's try to use unknownValue
+if (typeof unknownValue === 'number') {
+  // TypeScript knows that unknownValue is a string in this block
+  console.log(unknownValue.toFixed(2)); // OK
+}
+
+function runSomeCode() {
+  const random = Math.random();
+  if (random < 0.5) {
+    throw new Error('Something went wrong');
+  } else {
+    throw 'some error';
+  }
+}
+
+try {
+  runSomeCode();
+} catch (error) {
+  if (error instanceof Error) {
+    console.log(error.message);
+  } else {
+    console.log(error);
+    console.log('there was an error....');
+  }
+}
+
+
+// Type Never
+type Themes = 'light' | 'dark';
+
+function checkTheme(theme: Themes) {
+  if (theme === 'light') {
+    console.log('light theme');
+    return;
+  }
+  if (theme === 'dark') {
+    console.log('dark theme');
+    return;
+  }
+  theme;
+  // theme is of type never, because it can never have a value that is not 'light' or 'dark'.
+}
+
+enum Color {
+  Red,
+  Blue,
+  // Green,
+}
+
+function getColorName(color: Color) {
+  switch (color) {
+    case Color.Red:
+      return 'Red';
+    case Color.Blue:
+      return 'Blue';
+    default:
+      // at build time
+      let unexpectedColor: never = color;
+      // at runtime
+      throw new Error(`Unexpected color value: ${unexpectedColor}`);
+  }
+}
+
+console.log(getColorName(Color.Red)); // Red
+console.log(getColorName(Color.Blue)); // Blue
+// console.log(getColorName(Color.Green)); // Green
