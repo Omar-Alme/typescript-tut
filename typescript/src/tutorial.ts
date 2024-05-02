2ec34be55132415df722733505f226d9e8d1a529
Omar-Alme <omar.almehdar17@gmail.com>
2024-05-02 11:35:33 +0200

    fetch data, zod library

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -1237,7 +1237,7 @@ function processValue<T extends number | string>(value: T): T {
 
 
 // Generics default type
-interface StoreData<T> {
+interface StoreData<T = any> {
     data: T[];
 }
 
@@ -1248,3 +1248,61 @@ const storeNumbers: StoreData<number> = {
 const randomStuff:StoreData = {
     data:['random', 1]
 }
+
+// Gotcha  ZOD library to validate submissions
+import { z } from 'zod';
+
+// Fetch Data Axios and react query
+const url = 'https://www.course-api.com/react-tours-project';
+
+
+const tourSchema = z.object({
+id:z.string(),
+name:z.string(),
+info:z.string(),
+image:z.string(),
+price:z.string(),
+// randomthing: z.number() // doesnt exist so helps us check this at run time
+})
+
+// Fetch data setup data
+type Tour = z.infer<typeof tourSchema>;
+
+
+
+async function fetchData(url:string): Promise<Tour []>{
+    try{
+        const response = await fetch(url)
+        if(!response.ok){
+            throw new Error(`HTTP error! status:${response.status}`)
+        }
+        const rawData: Tour[] = await response.json()
+
+        const result = tourSchema.array().safeParse(rawData)
+        console.log(result);
+        
+        if(!result.success){
+            throw new Error(`Invalid data: ${result.error}`);
+        }
+
+        return result.data;
+    } catch (error){
+        const errorMsg =
+            error instanceof Error ? error.message : 'there was an error';
+        console.log(errorMsg);
+        return [];
+        
+    }
+}
+
+export{}
+
+const tours = await fetchData(url);
+tours.map((tour) => {
+    console.log(tour.name);
+        
+});
+
+
+
+// Gotcha  ZOD library to validate submissions
